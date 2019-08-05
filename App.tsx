e8f663d9108146bf3ce5e83c2a9e5e19aa178ccd
Medet Tleukabiluly <m.tleukabiluly@gmail.com>
2019-08-05 23:13:32 +0600

    add files

--- App.tsx
+++ App.tsx
@@ -7,9 +7,32 @@ export default function App () {
     <View style={styles.container}>
       <View style={styles.item}>
         <Image
-          style={styles.image}
+          style={{ width: 200 }}
           darken={0.6}
           darkenWidth={20}
+          source={require('./images/cat.png')}
+        />
+      </View>
+      <View style={styles.item}>
+        <Image
+          style={{ width: 300 }}
+          darken={0.8}
+          darkenWidth={30}
+          source={require('./images/cat.png')}
+        />
+      </View>
+      <View style={styles.item}>
+        <Image
+          style={{ width: 150 }}
+          darken={0.4}
+          darkenWidth={20}
+          source={require('./images/cat.png')}
+        />
+      </View>
+      <View style={styles.item}>
+        <Image
+          style={{ width: 100 }}
+          darken={.7}
           source={require('./images/cat.png')}
         />
       </View>
@@ -21,11 +44,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: 100
+    paddingTop: 60
   },
   item: {
-    height: 200,
-    width: 400
+    height: 150,
+    width: 400,
+    marginBottom: 20
   },
   image: {
     width: 200
